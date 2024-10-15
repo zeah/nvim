316c3a753ff913e01f779045b1eedc5f8dccb362
Arild jobb <tech@effektivmarkedsforing.no>
2024-10-15 10:32:55 +0200

    na

--- init.lua
+++ init.lua
@@ -10,3 +10,20 @@ vim.cmd.colorscheme("catppuccin")
 --   })
 -- end
 -- setup_lsp_diags()
+-- local function log_yanked_text(yanked_text)
+--   -- local log_file = "~/nvim/yanked_text.log" -- Change this to your desired file path
+--   local log_file = os.getenv("HOME") .. "/nvim/yanked_text.log" -- Use full path for the log file
+--   -- Escape the text to handle any special characters in shell commands
+--   yanked_text = vim.fn.escape(yanked_text, '"')
+--   -- Append the yanked text to the log file
+--   os.execute('echo "' .. yanked_text .. '" >> ' .. log_file)
+-- end
+--
+-- -- Autocommand for TextYankPost event
+-- vim.api.nvim_create_autocmd("TextYankPost", {
+--   callback = function()
+--     -- Get the text that was yanked
+--     local yanked_text = table.concat(vim.fn.getreg('"', 1, true), "\n")
+--     log_yanked_text(yanked_text)
+--   end,
+-- })
